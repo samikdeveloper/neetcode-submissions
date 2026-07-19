b12012a1ab84fbbfd5b82080a529cfb3da99cb10
Samik Chatterjee <59616286+samikdeveloper@users.noreply.github.com>
2026-07-19 12:23:54 +0530

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        s=s.toLowerCase().replaceAll("[^a-zA-Z0-9]","");
+        boolean flag=true;
+        int i=0,j=s.length()-1;
+        while(i<=j)
+        {
+            if(s.charAt(i)==s.charAt(j))
+            {
+                i++;j--;
+            }
+            else {
+                flag=false;
+                break;
+            }
+        }
+        return flag;
+    }
+}
