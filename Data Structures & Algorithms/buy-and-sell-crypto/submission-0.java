ddac166db69d65b9b181bab7e87bcef7839af8b6
Samik Chatterjee <59616286+samikdeveloper@users.noreply.github.com>
2026-08-16 01:10:36 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int buy=prices[0];
+        int max=0;
+        for(int i=1;i<prices.length;i++)
+        {
+            int currentProfit=prices[i]-buy;
+            if(currentProfit>max)
+            max=currentProfit;
+
+            if(prices[i]<buy)
+            buy=prices[i];
+        }
+        return max;
+    }
+}
